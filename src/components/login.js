2c9cea13a1663dafbada00fe1709fa56a73e5ef7
Paul <paul.ayling@bglgroup.co.uk>
2019-02-22 11:03:40 +0000

    WIP styling loging

--- src/components/login.js
+++ src/components/login.js
@@ -5,6 +5,7 @@ import {
   Link,
   Redirect
 } from "react-router-dom";
+import "../styling/lumen-bootstrap.css";
 
 class Login extends Component {
   constructor(props) {
@@ -48,31 +49,79 @@ class Login extends Component {
       return <Redirect to="/spaces" />;
     } else {
       return (
-        <div>
-          <form onSubmit={this.handleSubmit}>
-            Email: <br />
-            <input
-              type="text"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-            Password: <br />
-            <input
-              type="text"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-            />
-            <input type="submit" value="Submit" />
-          </form>
-          <button>
-            <Link to={`/users/new`}>Sign Up</Link>
-          </button>
-        </div>
+        <form>
+          <fieldset>
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input
+                type="email"
+                class="form-control"
+                placeholder="Enter email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input
+                type="password"
+                class="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+              />
+            </div>
+
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </fieldset>
+        </form>
       );
     }
   }
 }
 
 export default Login;
+
+//
+// return (
+//   <div>
+//     <form onSubmit={this.handleSubmit}>
+//       <div class="form-group row">
+//         <label for="email" class="col-sm-2 col-form-label">Email</label>
+//         <div class="col-sm-10">
+//           <input type="text" readonly="" class="form-control" name="email" value={this.state.email}
+//           onChange={this.handleChange}/>
+//         </div>
+//       </div>
+//
+//
+//       <div class="form-group row">
+//         <div class="col-sm-10">
+//           <label for="password" class="col-sm-2 col-form-label">
+//             Password
+//           </label>
+//         </div>
+//
+//         <div class="col-sm-10">
+//           <input
+//             type="text"
+//             name="password"
+//             value={this.state.password}
+//             onChange={this.handleChange}
+//           />
+//         </div>
+//       </div>
+//       <div class="form-group row">
+//         <input type="submit" value="Submit" class="btn btn-danger" />
+//       </div>
+//     </form>
+//
+//     <div class="form-group row">
+//       <button class="btn btn-danger">
+//         <Link to={`/users/new`}>Sign Up</Link>
+//       </button>
+//     </div>
+//   </div>
+// );
